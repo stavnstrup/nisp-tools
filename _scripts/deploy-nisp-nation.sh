d7bf2685cf5ad8227f8f7f2ffa32e578925edd4f
Jens Stavnstrup <stavnstrup@gmail.com>
2017-09-03 10:01:43 +0200

    Enable extension function
    For some strange reason the extension functions are disabled by default
    in the Saxon B XSLT 2.0 processor that is packaged for Ubuntu/Debian.

--- _scripts/deploy-nisp-nation.sh
+++ _scripts/deploy-nisp-nation.sh
@@ -10,4 +10,4 @@ git clone https://${GH_TOKEN}@github.com/stavnstrup/nisp-nation.git ../nisp-nati
 cd ../nisp-nation.master
 
 # Generate yaml and json representation of NISP database elements
-saxonb-xslt ../nisp-tools/src/standards/standards.xml debug.xsl
+saxonb-xslt -ext:on ../nisp-tools/src/standards/standards.xml debug.xsl
