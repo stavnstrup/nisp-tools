c283529eededfe011967ee7151f40ca2b1362639
Jens Stavnstrup <stavnstrup@gmail.com>
2017-09-03 09:39:30 +0200

    Add nisp-nation deployment step

---
+++ _scripts/deploy-nisp-nation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Deployment to nisp.nw3.dk
+
+# Enable error reporting to the console.
+set -e
+
+# Checkout Web generation tool package
+git clone https://${GH_TOKEN}@github.com/stavnstrup/nisp-nation.git ../nisp-nation.master
+cd ../nisp-nation.master
+
+# Generate yaml and json representation of NISP database elements
+saxonb-xslt ../nisp-tools/src/standards/standards.xml debug.xsl
