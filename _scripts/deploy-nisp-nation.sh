5935b4465a0e2ced822243b505eab37581b4d533
Jens Stavnstrup <stavnstrup@gmail.com>
2017-09-04 09:25:36 +0200

    Stylesheet have been renamed

--- _scripts/deploy-nisp-nation.sh
+++ _scripts/deploy-nisp-nation.sh
@@ -10,7 +10,7 @@ git clone https://${GH_TOKEN}@github.com/stavnstrup/nisp-nation.git ../nisp-nati
 cd ../nisp-nation.master
 
 # Generate yaml and json representation of NISP database elements
-saxonb-xslt -ext:on ../nisp-tools/src/standards/standards.xml debug.xsl
+saxonb-xslt -ext:on ../nisp-tools/src/standards/standards.xml _xsl/db2data.xsl
 
 # Build website
 bundle install
