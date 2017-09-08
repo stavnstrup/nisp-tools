a5196bbe477d0276993dea22ce5b4686619a8ebb
Jens Stavnstrup <stavnstrup@gmail.com>
2017-09-08 11:07:09 +0200

    Notify NISP-Nation of a change in the repository instead of building it ourselves

---
+++ _scripts/trigger-site-build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+body='{
+"request": {
+   "message": "Build requested by NISP-tools",
+   "branch": "master"
+}}'
+
+curl -s -X POST \
+   -H "Content-Type: application/json" \
+   -H "Accept: application/json" \
+   -H "Travis-API-Version: 3" \
+   -H "Authorization: token $TRAVIS_API_TOKEN" \
+   -d "$body" \
+   https://api.travis-ci.org/repo/stavnstrup%2Fnisp-nation/requests
