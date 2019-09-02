aa3e0c94c8bdc720603216029e817379fb0f1e63
Jens Stavnstrup <stavnstrup@gmail.com>
2019-09-02 22:30:49 +0200

    Create initial script for C3 taxonomy extract

---
+++ extra/python/make-c3.py
@@ -0,0 +1,9 @@
+# Extract C3 Taxonomy from em.pickle
+
+from SemanticDataset import *
+from SemanticHierarchy import *
+
+
+pages = loadDataset("em")
+
+# Create an XML fragment for use in the upcoming NISP 13
